8acdbdc64bb0b74239a44898ecb32ef2a0fc647f
Sion Smith <sion.smith@gmail.com>
2022-08-12 15:38:35 +0100

    udpates to make it work with Cp ansible 7.2.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp /root/staging/id_rsa /root/.ssh
-cp /root/staging/ansible-inventory.yml /ansible/playbooks/cp-ansible/inventories
+cp /root/staging/ansible-inventory.yml /ansible/ansible_collections/confluent/platform/inventories
 chmod 400 /root/.ssh/id_rsa
-/ansible/bin/ansible-playbook /ansible/playbooks/cp-ansible/all.yml
+/usr/bin/ansible-playbook -i /ansible/ansible_collections/confluent/platform/inventories/ansible-inventory.yml confluent.platform.all
